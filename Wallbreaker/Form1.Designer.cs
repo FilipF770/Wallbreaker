b9f3be342309a3db958760056d27770bb1b3cedd
ancablahova <ancablahova@post.cz>
2024-04-10 09:54:03 +0200

    Vytvoření projektu, přidání grafik, bitmapy, hlavního plátna, souřadnic.

--- Wallbreaker/Form1.Designer.cs
+++ Wallbreaker/Form1.Designer.cs
@@ -28,13 +28,59 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
-			this.components = new System.ComponentModel.Container ();
+			this.components = new System.ComponentModel.Container();
+			this.pbPlatno = new System.Windows.Forms.PictureBox();
+			this.btTest = new System.Windows.Forms.Button();
+			this.tmrRedraw = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pbPlatno
+			// 
+			this.pbPlatno.BackColor = System.Drawing.Color.White;
+			this.pbPlatno.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pbPlatno.Location = new System.Drawing.Point(12, 12);
+			this.pbPlatno.Name = "pbPlatno";
+			this.pbPlatno.Size = new System.Drawing.Size(525, 426);
+			this.pbPlatno.TabIndex = 0;
+			this.pbPlatno.TabStop = false;
+			// 
+			// btTest
+			// 
+			this.btTest.Location = new System.Drawing.Point(584, 12);
+			this.btTest.Name = "btTest";
+			this.btTest.Size = new System.Drawing.Size(85, 27);
+			this.btTest.TabIndex = 1;
+			this.btTest.Text = "button1";
+			this.btTest.UseVisualStyleBackColor = true;
+			this.btTest.Click += new System.EventHandler(this.btTest_Click);
+			// 
+			// tmrRedraw
+			// 
+			this.tmrRedraw.Tick += new System.EventHandler(this.tmrRedraw_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size (800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btTest);
+			this.Controls.Add(this.pbPlatno);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Wall Breaker";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pbPlatno;
+		private System.Windows.Forms.Button btTest;
+		private System.Windows.Forms.Timer tmrRedraw;
 	}
 }
 
--- Wallbreaker/Form1.cs
+++ Wallbreaker/Form1.cs
@@ -12,9 +12,65 @@ namespace Wallbreaker
 {
 	public partial class Form1 : Form
 	{
+		// Bitmapa na kreslení
+		Bitmap mobjBtmp;
+		Graphics mobjPlatnoNaPozadi;
+
+		// Grafika na PictureBoxu
+		Graphics mobjPredniPlatno;
+
+		// Souřadnice kuličky
+		int mintKulickaX, mintKulickaY;
+		
+		// Konstruktor
 		public Form1 ()
 		{
 			InitializeComponent ();
+		}
+
+		/// <summary>
+		/// Nahrání formu do paměti
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_Load (object sender, EventArgs e)
+		{
+			// Vytvoření grafiky z PictureBoxu
+			mobjPredniPlatno = pbPlatno.CreateGraphics ();
+
+			// Vytvoření kompatibilní bitmapy a grafiky
+			mobjBtmp = new Bitmap (pbPlatno.Width, pbPlatno.Height);
+			mobjPlatnoNaPozadi = Graphics.FromImage (mobjBtmp);
+
+			// Nastavení kuličky
+			mintKulickaX = mintKulickaY = 10;
+
+			// Nastavení Timeru pro překreslení
+			tmrRedraw.Interval = 500;
+			tmrRedraw.Enabled = true;
+		}
+
+		private void btTest_Click (object sender, EventArgs e)
+		{
+			
+		}
+
+		/// <summary>
+		/// Překreslení obrazu
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void tmrRedraw_Tick (object sender, EventArgs e)
+		{
+			// Nakreslení kolečka
+			mobjPlatnoNaPozadi.FillEllipse (Brushes.Green, mintKulickaX, mintKulickaY, 30, 30);
+
+			// Posun kuličky
+			mintKulickaX = mintKulickaX + 5;
+			mintKulickaY = mintKulickaY + 5;
+
+			// Nakopírování obrázku na PictureBox
+			mobjPredniPlatno.DrawImage (mobjBtmp, 0, 0);
 		}
 	}
 }
